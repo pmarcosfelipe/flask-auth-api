0132f28480ad76e6c971ecdd8b0f359cac16103d
Marcos Felipe <p.marcosfelipe@gmail.com>
2024-10-15 08:46:37 +0100

    created login method integrated with Flask Login

--- app.py
+++ app.py
@@ -1,12 +1,40 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from models.user import User
 from database import db
+from flask_login import LoginManager,login_user, current_user
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "your_secret_key"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 
+login_manager = LoginManager()
+
 db.init_app(app)
+
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+
+@login_manager.user_loader
+def load_user(userId):
+  return User.query.userId
+
+@app.route("/login", methods=["POST"])
+def login():
+  data = request.json
+  username = data.get("username")
+  password = data.get("password")
+
+  if username and password:
+    user = User.query.filter_by(username=username).first()
+
+    if user and user.password == password:
+      login_user(user)
+      print(f"current user: {current_user}")
+      return jsonify({"message": "Authentication successfully done!"}), 200
+
+  return jsonify({"message": "Invalid credentials!"}), 400
+ 
 
 @app.route("/", methods=["GET"])
 def hello_world():
